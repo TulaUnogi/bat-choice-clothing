8d779584fea6ff2fb2848a4fa57745d632302ad3
Karolina Piech <karolinapiech1990@gmail.com>
2024-02-07 01:09:05 +0000

    Add products pt 1

--- products/models.py
+++ products/models.py
@@ -21,6 +21,7 @@ class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     name = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField()
+    condition = models.CharField(max_length=200, null=True, blank=True)
     size = models.CharField(max_length=20, null=True, blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     image = models.ImageField(null=True, blank=True)
