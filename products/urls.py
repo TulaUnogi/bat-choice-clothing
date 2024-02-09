eae389f99707b2da60b1ed496346b000234d19c2
Karolina Piech <karolinapiech1990@gmail.com>
2024-02-09 00:59:52 +0000

    Add and style product details page

--- products/urls.py
+++ products/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.products_all, name='products'),
+    path('<product_id>', views.product_details, name='product_details'),
 ]
  
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -13,3 +13,16 @@ def products_all(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_details(request, product_id):
+    """ Single product details view """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
+
