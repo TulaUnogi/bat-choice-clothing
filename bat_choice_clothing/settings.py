59af5383614cf8bf6e7d2a2a98f9512da7425132
Karolina Piech <karolinapiech1990@gmail.com>
2024-02-11 23:51:25 +0000

    Set up database and env.py vars

--- bat_choice_clothing/settings.py
+++ bat_choice_clothing/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -21,13 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+ujm_i*y2#b*6*)ryz28h*x(rr0sb$5if$)qn=wc6ft1jh2o6e'
+# Secret key
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# Setting up debug
+DEBUG = 'DEVELOPMENT' in os.environ
 
-ALLOWED_HOSTS = ['8000-tulaunogi-batchoiceclot-unahjr31hk4.ws-eu108.gitpod.io',]
+ALLOWED_HOSTS = ['8000-tulaunogi-batchoiceclot-6rs6vrwdjeg.ws-eu108.gitpod.io',]
 
 
 # Application definition
@@ -115,12 +118,17 @@ WSGI_APPLICATION = 'bat_choice_clothing.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    }
 
 
 # Password validation
