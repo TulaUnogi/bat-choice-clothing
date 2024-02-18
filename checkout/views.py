a414f06aced110a4cae1597bfbf3270bb07e8f7f
Karolina Piech <karolinapiech1990@gmail.com>
2024-02-18 23:23:19 +0000

    Update checkout to render order & form

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
+
 
 
 def checkout(request):
     """ A view to return the checkout page """
+
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, 'Oops! Nothing in your bag yet!')
+        return redirect(reverse('products'))
     
-    return render(request, "checkout/checkout.html")
+    order_form = OrderForm()
+    template = "checkout/checkout.html"
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
