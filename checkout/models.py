7a4eaf6ffba6c9b723f8e857a7e25eca224698b7
Karolina Piech <karolinapiech1990@gmail.com>
2024-02-18 20:45:09 +0000

    Combine OrderLine with OrderLineItem and tidy up

--- checkout/models.py
+++ checkout/models.py
@@ -6,7 +6,7 @@ import uuid
 
 
 class Order(models.Model):
-    """ Stores all customer order data """
+    """Stores all customer order data"""
     
     order_date_time = models.DateTimeField(auto_now_add=True)
     first_name = models.CharField(max_length=50, null=False, blank=False)
@@ -20,7 +20,9 @@ class Order(models.Model):
     city = models.CharField(max_length=85, null=False, blank=False)
     postcode = models.CharField(max_length=30, null=True, blank=True)
     country = models.CharField(max_length=56, null=False, blank=False)
-    order_number = models.UUIDField(default=uuid.uuid4, unique=True, db_index=True, editable=False)
+    order_number = models.UUIDField(
+        default=uuid.uuid4, unique=True, db_index=True, editable=False
+    )
     order_status = models.CharField(max_length=40, null=False, blank=False)
 
     def __str__(self):
@@ -36,68 +38,61 @@ class Order(models.Model):
         return self.grand_total
 
 
-class Discount(models.Model):
-    """ Stores active discount codes and their values """
-
-    code = models.CharField(max_length=30, null=True, blank=True)
-    percent = models.PositiveIntegerField(null=True, default=0)
-    is_active = models.BooleanField(default=False)
-
-    def __str__(self):
-        return f'{self.discount_percent}% off with code: {self.discount_code}'
-
-
 class OrderLineItem(models.Model):
-    """ Stores ordered product data """
-    order_item = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    """
+    Stores ordered product data
+    & data related to calculated order totals
+    """
+
+    order_line = models.ForeignKey(
+        Order, null=False, blank=False, on_delete=models.CASCADE,
+        related_name="lineitems",
+    )
+    product = models.ForeignKey(
+        Product, null=False, blank=False, on_delete=models.CASCADE
+    )
     product_quantity = models.IntegerField(default=1, null=False) #single items only
-
-    def save(self, *args, **kwargs):
-        """ Overrides the default save to set total """
-        self.product_total = self.product.price * self.quantity
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.product_total
-
-
-class OrderLine(models.Model):    
-    """ Stores data related to calculated order totals """
-
-    order_line = models.ForeignKey(OrderLineItem, null=False, blank=False, 
-                                   on_delete=models.CASCADE, related_name='lineitems')
-    order = models.ForeignKey(Order, null=False, blank=False, 
-                                         on_delete=models.CASCADE)
-    order_subtotal = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    discount = models.OneToOneField(Discount, on_delete=models.CASCADE, 
-                                           related_name='discount') # one code per order
-
-    order_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    shipping_cost = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
-    grand_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, default=0)
+    order_subtotal = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, default=0
+    )
+    discount = models.OneToOneField(
+        "Discount", on_delete=models.CASCADE, related_name="discount"
+    )  # one code per order
+    order_total = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, default=0
+    )
+    shipping_cost = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, default=0
+    )
+    grand_total = models.DecimalField(
+        max_digits=6, decimal_places=2, null=False, default=0
+    )
 
     def update_order_subtotal(self):
 
         # Calculate order subtotal (from Code Institute Boutique Ado walktrough)
-        self.order_subtotal = self.lineitems.aggregate(Sum('product_total'))['product_total__sum']
+        self.order_subtotal = self.lineitems.aggregate(Sum("product_total"))[
+            "product_total__sum"
+        ]
 
-    def update_order_total(self):
+    def update_product_total(self):
+        self.product_total = self.product.price * self.quantity
 
+    def update_discounted_total(self):
         # Calculate discount value
         if self.discount.code:
             discount_value = (self.subtotal * self.discount.percent) / 100
         else:
             discount_value = 0
-
         # Calculate total
         self.order_total = self.order_subtotal - discount_value
 
     def update_shipping_cost(self):
         # Calculate shipping costs
         if self.order_subtotal < settings.FREE_SHIPPING_THRESHOLD:
-            self.shipping_cost = self.order_subtotal * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            self.shipping_cost = (
+                self.order_subtotal * settings.STANDARD_DELIVERY_PERCENTAGE / 100
+            )
         elif self.shipping_cost < settings.MINIMUM_SHIPPING_COST:
             self.shipping_cost = settings.MINIMUM_SHIPPING_COST
         else:
@@ -108,22 +103,38 @@ class OrderLine(models.Model):
         self.grand_total = self.order_total + self.delivery_cost
 
     def create_order_number(self):
-        """ Use UUID to create a unique order number"""
+        """Use UUID to create a unique order number"""
 
         return uuid.uuid4().hex.upper()
 
     def save(self, *args, **kwargs):
-        """ 
+        """
         Override the default save to set the order number if it wasn't there yet.
         From Boutique Ado walktrough.
         """
 
         if not self.order_number:
             self.order_number = self._create_order_number()
-            super().save(*args, **kwargs)
-
-        if not self.order_subtotal:
-            self.order_subtotal = self.update_order_subtotal()
+        elif not self.order_subtotal:
+            self.order_subtotal = self.update_order_total()
+        elif not self.order_total:
+            self.order_total = self.update_order_total()
+        elif not self.order_s:
+            self.shipping_cost = self.update_shipping_cost()
+        elif not self.order_total:
+            self.grand_total = self.update_grand_total()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return f'Order grand total: {self.grand_total}€'
+
+
+class Discount(models.Model):
+    """Stores active discount codes and their values"""
+
+    code = models.CharField(max_length=30, null=True, blank=True)
+    percent = models.PositiveIntegerField(null=True, default=0)
+    is_active = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'{self.discount_percent}% off with code: {self.discount_code}'
