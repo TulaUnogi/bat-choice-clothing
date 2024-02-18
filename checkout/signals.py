80a78ce79d900454020bbff476c1c21c67dcab66
Karolina Piech <karolinapiech1990@gmail.com>
2024-02-18 22:37:26 +0000

    Add forms and signals

---
+++ checkout/signals.py
@@ -0,0 +1,16 @@
+from django.db.models import post_save, post_delete
+from django.dispatch import receiver
+from .models import OrderLineItem
+
+
+@receiver(post_save, sender=OrderLineItem)
+def update_total_on_save(sender, instance, created, **kwargs):
+    """Update order total when lineitem is created"""
+
+    instance.order.update_order_subtotal()
+
+@receiver(post_delete, sender=OrderLineItem)
+def update_total_on_delete(sender, instance, **kwargs):
+    """Update order total when lineitem is deleted"""
+
+    instance.order.update_order_subtotal()
