ef6463a4919ad556b28fe7a25aec6d6c1b867085
Karolina Piech <karolinapiech1990@gmail.com>
2024-04-04 19:52:46 +0000

    Add Stripe card element

--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 from django.contrib import messages
+from django.conf import settings
 from .forms import OrderForm
-
+import stripe
 
 
 def checkout(request):
@@ -16,6 +17,9 @@ def checkout(request):
     template = "checkout/checkout.html"
     context = {
         'order_form': order_form,
+        'stripe_public_key': 
+                            'pk_test_51P1EyzRtag8MNdhxl3LG8hxyFeb3K2flCNNNRllvw1X12uG78gpAu0BVaQD6YgpOna3EK4TaK6BJnYyCwxXWDwRV00HWLSVWut',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
