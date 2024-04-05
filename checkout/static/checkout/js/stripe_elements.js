82e711ef7889357307fd2f99f6ae33b36a457862
Karolina Piech <karolinapiech1990@gmail.com>
2024-04-05 20:50:18 +0000

    Add card validation error handling and fix font styling

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -6,15 +6,24 @@
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripePublicKey);
-var elements = stripe.elements();
+var elements = stripe.elements({
+    fonts: [
+      {
+        // integrate your font into stripe
+        cssSrc: 'https://fonts.googleapis.com/css2?family=Megrim&family=Montserrat:ital,wght@0,100;0,400;0,700;1,400&display=swap',
+      }
+    ]
+  });;
+
 var style = {
     base: {
         color: '#495057',
         fontSmoothing: 'antialiased',
         fontFamily: '"Montserrat", sans-serif',
-        fontSize: '1rem',
+        fontWeight: '400',
+        fontSize: '16px',
         '::placeholder': {
-            color: '#aab7c4'
+            color: '#aab7c4',
         }
     },
     invalid: {
@@ -24,3 +33,21 @@ var style = {
 };
 var card = elements.create('card', {style: style});
 card.mount('#card-element');
+
+
+/* Card validation errors handling */
+
+card.addEventListener('change', function (event) {
+    var errorDiv = document.getElementById('card-errors');
+    if (event.error) {
+        var html = `
+            <span class="icon" role="alert">
+                <i class="fas fa-times"></i>
+            </span>
+            <span>${event.error.message}</span>
+        `;
+        $(errorDiv).html(html);
+    } else {
+        errorDiv.textContent = '';
+    }
+});
