d6cffd70f4be09ffa81dbf1e18f62e26d4a711dd
Karolina Piech <karolinapiech1990@gmail.com>
2024-04-08 23:13:09 +0000

    Add success page for checkout

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.checkout, name='checkout'),
+    path('success/<order_number>', views.success_checkout_page, name='success_checkout_page'),
 ]
