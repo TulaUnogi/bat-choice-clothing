1a3385d0126048a22c2575fef1ee6de513fa18f6
Karolina Piech <karolinapiech1990@gmail.com>
2024-04-26 02:22:59 +0000

    Add placeholders and fix category names

--- products/forms.py
+++ products/forms.py
@@ -6,9 +6,36 @@ class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = '__all__'
+        fields = (
+            'category', 'name', 'description',
+            'condition', 'size', 'price',
+            'primary_colour', 'secondary_colour',
+            'image',
+        )
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
-        display_names = [(c.id, c.display_name()) for c in categories]
+        display_names = [(c.display_name()) for c in categories]
+
+        placeholders = {
+            'category': 'Category',
+            'name': 'Product Name',
+            'description': 'Description',
+            'condition': 'Condition of the product',
+            'size': 'Size',
+            'price': 'Price',
+            'primary_colour': 'Primary Colour',
+            'secondary_colour': 'Secondary Colour',
+            'image': 'Product Image'
+        }
+
+        self.fields['category'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if field != 'category':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].label = False
