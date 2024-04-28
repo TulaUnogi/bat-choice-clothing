47da04dc301568860aa4e6ad3b49201044dc3630
Karolina Piech <karolinapiech1990@gmail.com>
2024-04-28 15:51:01 +0000

    Add product delete url and insert 'int:' in a correct way

--- products/urls.py
+++ products/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.products_all, name='products'),
-    path('<product_id>/', views.product_details, name='product_details'),
+    path('<int:product_id>/', views.product_details, name='product_details'),
     path('add/', views.add_product, name='add_product'),
-    path('<product_id>/edit', views.edit_product, name='edit_product'),
-
+    path('<int:product_id>/edit', views.edit_product, name='edit_product'),
+    path('<int:product_id>/delete', views.delete_product, name='delete_product'),
 ]
  
