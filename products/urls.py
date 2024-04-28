bc6685bba35ae2b0ba851242d2e8e5d46b639fac
Karolina Piech <karolinapiech1990@gmail.com>
2024-04-28 15:39:39 +0000

    Add edit_product url and remove 'int:' from product_details url

--- products/urls.py
+++ products/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.products_all, name='products'),
-    path('int:<product_id>/', views.product_details, name='product_details'),
+    path('<product_id>/', views.product_details, name='product_details'),
     path('add/', views.add_product, name='add_product'),
+    path('<product_id>/edit', views.edit_product, name='edit_product'),
+
 ]
  
