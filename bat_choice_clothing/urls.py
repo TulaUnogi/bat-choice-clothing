f0700a1d9fd0efed3be6ba168137ccd96cec4222
Karolina Piech <karolinapiech1990@gmail.com>
2024-04-30 18:35:31 +0000

    Add custom error pages

--- bat_choice_clothing/urls.py
+++ bat_choice_clothing/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import handler404, handler500
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
@@ -29,3 +31,6 @@ urlpatterns = [
     path('checkout/', include('checkout.urls')),
     path('my-profile/', include('userprofile.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+handler404 = 'bat_choice_clothing.views.handler404'
+handler500= 'bat_choice_clothing.views.handler500'
