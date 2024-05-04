e889dcb457c7fa1f712f8136ca7bad7b6c23743f
Karolina Piech <karolinapiech1990@gmail.com>
2024-05-04 21:03:58 +0000

    Fix shipping logic and tidy up the code

--- checkout/models.py
+++ checkout/models.py
@@ -68,7 +68,7 @@ class Order(models.Model):
         ] or 0
         self.save()
         return self.order_subtotal
- 
+
     def update_discounted_total(self):
         # Calculate discount value
         if self.discount:
@@ -84,16 +84,16 @@ class Order(models.Model):
     def update_shipping_cost(self):
         # Calculate shipping costs
         if self.order_subtotal < settings.FREE_SHIPPING_TRESHOLD:
-            self.shipping_cost = (self.order_subtotal 
-                                  * Decimal(settings.STANDARD_SHIPPING_PERCENTAGE
-                                            / 100))
-        elif self.shipping_cost < settings.MINIMUM_SHIPPING_COST:
-            self.shipping_cost = settings.MINIMUM_SHIPPING_COST
+            if self.order_subtotal >= settings.MINIMUM_SHIPPING_COST:
+                self.shipping_cost = (self.order_subtotal 
+                                    * Decimal(settings.STANDARD_SHIPPING_PERCENTAGE
+                                                / 100))
+                if self.shipping_cost < settings.MINIMUM_SHIPPING_COST:
+                    self.shipping_cost = settings.MINIMUM_SHIPPING_COST
         else:
             self.shipping_cost = 0
         self.save()
         return self.shipping_cost
-        
 
     def update_grand_total(self):
         # Calculate grand total
@@ -125,29 +125,29 @@ class OrderLineItem(models.Model):
     """
     Stores ordered product data
     """
-
     order = models.ForeignKey(Order, null=False, blank=False, 
-                              on_delete=models.CASCADE, related_name="lineitems",)
-    product = models.ForeignKey(Product, null=False, blank=False, 
+                              on_delete=models.CASCADE, related_name="lineitems")
+    product = models.ForeignKey(Product, null=False, blank=False,
                                 on_delete=models.CASCADE)
-    product_total = models.DecimalField(max_digits=6, decimal_places=2, 
+    product_total = models.DecimalField(max_digits=6, decimal_places=2,
                                         null=False, default=0)
     product_quantity = models.IntegerField(default=1, null=False) #single items only
-    
+
     def save(self, *args, **kwargs):
         """
         Override default save function to set the product_total.
         """
         self.product_total = self.product.price * self.product_quantity
         super().save(*args, **kwargs)    
-    
+
 
 class Discount(models.Model):
     """Stores active discount codes and their values"""
 
-    discount_code = models.CharField(max_length=30, null=True, blank=True)
+    discount_code = models.CharField(max_length=30, null=True, blank=True,
+        	                         unique=True)
     percent = models.PositiveIntegerField(null=True, default=0)
     is_active = models.BooleanField(default=False)
 
     def __str__(self):
-        return f'{self.percent}% off with code: {self.code}'
+        return f'{self.percent}% off with code: {self.discount_code}'
