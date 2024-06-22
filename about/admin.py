9930cdae05f409e30ffb863b13a30f9872d654e2
Karolina Piech <karolinapiech1990@gmail.com>
2024-06-22 21:25:09 +0000

    Add ReviewAdmin class for better reviews display in admin

--- about/admin.py
+++ about/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from .models import Review
 
-admin.site.register(Review)
+
+class ReviewAdmin(admin.ModelAdmin):
+    readonly_fields = [
+        'customer', 'order', 'rating', 'review_date', 'order_review',
+    ]
+
+    fields = [
+        'customer', 'order', 'rating', 'review_date', 'order_review',
+    ]
+
+    list_display = [
+        'customer', 'order', 'rating', 'review_date', 'order_review',
+    ]
+
+    ordering = ('-review_date',)
+
+admin.site.register(Review, ReviewAdmin)
