f9574fbc871f8432558f71a9809601aac9ae5891
Karolina Piech <karolinapiech1990@gmail.com>
2024-06-30 20:15:16 +0000

    Add phone number validation

--- userprofile/models.py
+++ userprofile/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.db.models.signals import post_save, pre_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
+from django.core.validators import RegexValidator
 
 from django_countries.fields import CountryField
+
 
 
 class UserProfile(models.Model):
@@ -14,7 +16,11 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     saved_full_name = models.CharField(max_length=100, null=True, blank=True)
     saved_email = models.EmailField(max_length=300, null=True, blank=True)
-    saved_phone_number = models.CharField(max_length=17, blank=True, null=True)
+    saved_phone_number = models.CharField(
+        max_length=16, blank=True, null=True, validators=[RegexValidator(
+            regex=r'^\+?1?\d{9,15}$', 
+            message="Phone number must be entered in the format: '+123456789'.\
+                     Up to 15 digits allowed.")])
     saved_address_line1 = models.CharField(max_length=200, null=True, blank=True)
     saved_address_line2 = models.CharField(max_length=200, null=True, blank=True)
     saved_region = models.CharField(max_length=85, null=True, blank=True)
