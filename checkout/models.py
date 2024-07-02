8226b8e5c3aa7023ba7632121dc3f6144b5ea27c
Karolina Piech <karolinapiech1990@gmail.com>
2024-07-02 23:50:30 +0000

    Add validation for phone & postcode

--- checkout/models.py
+++ checkout/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.db.models import Sum
 from django.conf import settings
+from django.core.validators import RegexValidator
 
 from products.models import Product
 from userprofile.models import UserProfile
@@ -32,14 +33,23 @@ class Order(models.Model):
                                       null=True, blank=True, related_name='orders')
     full_name = models.CharField(max_length=100, null=False, blank=False, default="")
     email = models.EmailField(max_length=300, null=False, blank=False)
-    phone_number = models.CharField(max_length=17, blank=True, null=True)
+    phone_number = models.CharField(
+        max_length=17, blank=True, null=True, validators=[RegexValidator(
+            regex=r'^\+?1?\d{9,15}$', 
+            message="Phone number must be entered in the format: '+123456789'.\
+                     Up to 15 digits allowed.",
+            code="invalid")])
     address_line1 = models.CharField(max_length=200, null=False, blank=False, 
                                      default="")
     address_line2 = models.CharField(max_length=200, null=False, blank=False, 
                                      default="")
     region = models.CharField(max_length=85, null=False, blank=False)
     city = models.CharField(max_length=85, null=False, blank=False)
-    postcode = models.CharField(max_length=30, null=True, blank=True)
+    postcode = models.CharField(
+        max_length=30, null=True, blank=True, validators=[RegexValidator(
+            regex=r'^[\d\-a-zA-Z]+$', 
+            message="Only letters, numbers and hyphens allowed.",
+            code="invalid")])
     country = CountryField(blank_label="Country *", null=False, blank=False)
     order_number = models.CharField(max_length=32, null=False, unique=True,
                                     db_index=True, editable=False)
