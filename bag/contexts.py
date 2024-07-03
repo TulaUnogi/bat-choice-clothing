9cd0d3b83f15c9f6cea9ece1cc50a9b630b758ff
Karolina Piech <karolinapiech1990@gmail.com>
2024-07-03 20:59:36 +0000

    Remove unnecessary calculations

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,4 +1,3 @@
-from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
 
@@ -6,11 +5,9 @@ from products.models import Product
 
 
 def in_bag(request):
+    # Displays current bag contents and subtotal
     bag_items = []
     subtotal = 0
-    total = 0
-    shipping = 0
-    free_shipping_delta = 0
 
     product_count = 0
     bag = request.session.get("bag", {})
@@ -26,29 +23,11 @@ def in_bag(request):
                 "product": product,
             }
         )
-    # Calculate shipping
-    if subtotal < settings.FREE_SHIPPING_TRESHOLD:
-        free_shipping_delta = settings.FREE_SHIPPING_TRESHOLD - subtotal
-        if subtotal >= settings.MINIMUM_SHIPPING_COST:
-            shipping = subtotal * Decimal(settings.STANDARD_SHIPPING_PERCENTAGE / 100)
-            if shipping < settings.MINIMUM_SHIPPING_COST:
-                shipping = settings.MINIMUM_SHIPPING_COST
-    else:
-        shipping = 0
-        free_shipping_delta = 0
-
-    total = subtotal
-
-    grand_total = shipping + total
 
     context = {
         "bag_items": bag_items,
         "subtotal": subtotal,
         "product_count": product_count,
-        "shipping": shipping,
-        "free_shipping_delta": free_shipping_delta,
-        "free_shipping_treshold": settings.FREE_SHIPPING_TRESHOLD,
-        "grand_total": grand_total,
     }
 
     return context
