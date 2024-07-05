b72b9bf7e411b00bbb8ce8e36519f17e323f1a61
Karolina Piech <karolinapiech1990@gmail.com>
2024-07-05 08:54:15 +0000

    Add Products filtering in admin

--- products/admin.py
+++ products/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from .models import Category, Product
 
 
+
+class ProductAdmin(admin.ModelAdmin):
+
+    readonly_fields= (
+        'name', 'category', 'price', 'availability'
+    )
+
+    list_filter = ('availability','size', 'category')
+
+    ordering = ('-added_on',)
+
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
